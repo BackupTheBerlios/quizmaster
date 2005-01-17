b3b9d447c308859a46f0f3575ab5e3a3c55ec608
neo36 <neo36>
2005-01-17 01:31:43 +0000

    Der FileBuilder liest jetzt den kompletten Inhalt der bisherigen Quizdatei ein (falls sie schon existiert) und schreibt ihn neu aus, bevor man neue Fragen eingeben kann....

--- quizmaster/xml/FileBuilder.java
+++ quizmaster/xml/FileBuilder.java
@@ -6,9 +6,21 @@ package xml;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Vector;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import messaging.QuizQuestion;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * @author reinhard
@@ -16,7 +28,100 @@ import java.io.InputStreamReader;
  *
  */
 public class FileBuilder {
+	
+	private static Vector recentQuestions;
+	private static String filename;
+	
+	/**
+	 * This method checks if the specified file exists and backs up questions
+	 *
+	 */
+	private static void checkAndBackup()
+	{
+		File f = new File(filename);
+		if(!f.exists())
+		{
+			return;
+		}
+		
+		Document doc = null;
 
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc = db.parse(f);
+        } catch (java.io.IOException e) {
+            System.out.println("Can't find the file");
+        } catch (Exception e) {
+            System.out.println("Problem parsing the file.");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        }
+        
+        // Iterate over question elements
+    		NodeList questions = doc.getElementsByTagName("question");
+		
+    		int i=0;
+		
+    		for(i=0; i<questions.getLength(); i++)
+		{
+			Node question = questions.item(i);
+			
+			int questionID = Integer.parseInt(question.getAttributes().getNamedItem("id").getNodeValue());
+			
+			String questionText =  question.getAttributes().getNamedItem("text").getNodeValue();
+
+			NodeList answers = ((Element)question).getElementsByTagName("answer");
+			
+			Node[] anArray = new Node[4];
+			
+			for(int k=0; k<answers.getLength(); k++)
+			{
+				anArray[k]=answers.item(k);
+			}
+			
+			int points = Integer.parseInt(question.getAttributes().getNamedItem("points").getNodeValue());
+			
+			QuizQuestion q = new QuizQuestion(questionText);
+			q.setId(questionID);
+			q.setPoints(points);
+			
+			Vector a = new Vector();	
+			String[] antworten = new String[4];
+			
+			//Iterate over answer elements
+			for(int j=0; j<anArray.length; j++)
+			{				
+				boolean correct = false;
+				Node answer = anArray[j];
+				Node correctNode = answer.getAttributes().getNamedItem("correct");
+				
+				if(correctNode!=null)
+				{
+					correct = Boolean.valueOf(correctNode.getNodeValue()).booleanValue();
+				}
+				
+				String ans = answer.getFirstChild().getNodeValue();
+				
+				a.add(ans);
+
+				if(correct)
+				{
+					// HACK
+					q.setCorrectAnswer(j+1);
+				}
+				
+			}	
+			q.setAnswers(a);
+			recentQuestions.add(q);
+		}
+	}
+
+
+	/**
+	 * The main method does all the work
+	 * @param args
+	 */
 	public static void main(String[] args) {
 	
 		if(args.length!=1)
@@ -27,12 +132,17 @@ public class FileBuilder {
 			return;
 		}
 		
+		filename = args[0];
+		
 		int id=0;
+		
+		recentQuestions = new Vector();
+		checkAndBackup();
 		
 		FileWriter fileout=null;
 
 		try {
-			fileout = new FileWriter(args[0]);
+			fileout = new FileWriter(filename);
 		} catch (IOException e1) {
 			System.err.println(e1.getMessage());
 			e1.printStackTrace();
@@ -45,6 +155,45 @@ public class FileBuilder {
 			out.write("<quiz>\n");
 		} catch (IOException e3) {
 			e3.printStackTrace();
+		}
+		
+		if(recentQuestions.size()>0)
+		{
+			for(int i=0; i<recentQuestions.size(); i++)
+			{
+				QuizQuestion theQuestion = (QuizQuestion) recentQuestions.elementAt(i);
+				String question="\t<question id=\""+theQuestion.getId()+
+								"\" text=\""+theQuestion.getQuestion()+"\" points=\""+
+								theQuestion.getPoints()+"\">\n";
+			
+				try {
+					out.write(question);
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+				
+				for(int j=0; j < theQuestion.getAnswers().size(); j++)
+				{
+					String answer="\t\t<answer";
+					if(theQuestion.getCorrectAnswer()==j)
+					{
+						answer+=" correct=\"true\"";
+					}
+					answer=answer+">"+(String) theQuestion.getAnswers().elementAt(j)+"</answer>\n";
+					
+					try {
+						out.write(answer);
+					} catch (IOException e4) {
+						e4.printStackTrace();
+					}
+				}
+				
+				try {
+					out.write("\t</question>\n");
+				} catch (IOException e4) {
+					e4.printStackTrace();
+				}
+			}
 		}
 
 		do {
