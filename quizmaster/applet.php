adb604bee3793cc8d3654198027ae02e8498fa74
thetable <thetable>
2005-01-19 21:41:57 +0000

    made paths relative

--- quizmaster/applet.php
+++ quizmaster/applet.php
@@ -4,8 +4,7 @@
 
 <html>
 <body>
-
-<APPLET CODEBASE="http://192.168.2.10/~reinhard/" CODE="client.ClientApplet.class" WIDTH=800 HEIGHT=600>
+<APPLET CODEBASE="localhost/quizmaster/" CODE="client.ClientApplet.class" WIDTH=800 HEIGHT=600>
 	<param name="nickname" value=<?php echo "\"".$username."\""; ?>">
 </APPLET>
 </body>
