8771182e3522427e81ed886014c03dd48eaff3a1
neo36 <neo36>
2005-01-26 19:21:14 +0000

    Initial JDBC support, doesn't work, yet, but will...

--- quizmaster/server/HighScore.java
+++ quizmaster/server/HighScore.java
@@ -4,6 +4,11 @@
  */
 package server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Vector;
 
 /**
@@ -17,15 +22,33 @@ public class HighScore
 	private final int MAXENTRIES = 10;
 	private int lowestScore;
 	
+	// For the database
+	private String dbHost = "localhost";
+	private String dbName = "highscore";
+	private String dbUser = "quizmaster";
+	private String dbPassword = "quizpass";
+	private String dbUri = "jdbc:mysql://" + dbHost + "/" + dbName + "?user=" + dbUser + "&password=" + dbPassword;
+	private Connection con;
+	
 	/**
 	 * Constructor
 	 *
 	 */
-	public HighScore()
+	public HighScore() throws Exception
 	{
 		this.highscore = new Vector( MAXENTRIES );
 		this.lowestScore = 0;
 		this.loadHighscore();
+		
+		Class.forName("org.gjt.mm.mysql.Driver");
+		con = DriverManager.getConnection(this.dbUri);
+		
+		try{
+			this.loadHighscore();
+		} catch(SQLException e){
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -49,7 +72,6 @@ public class HighScore
 	 */
 	private void addEntry(String nick, int points)
 	{
-		
 		if(this.highscore.isEmpty())
 		{
 			this.highscore.add(nick);
@@ -81,27 +103,49 @@ public class HighScore
 		// Update lowest score
 		this.setLowestScore( ((Integer) this.highscore.elementAt(this.highscore.size()-1)).intValue() );
 		
-		this.saveHighscore();
+		try{
+			this.saveHighscore();
+		} catch(SQLException e)
+		{
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	/**
-	 * Method for saving Highscore data
+	 * Method for saving Highscore data to the database
 	 *
 	 */
-	public void saveHighscore()
+	public void saveHighscore() throws SQLException
 	{
-		// empty
+		Statement s = con.createStatement();
+		
+		for(int i=0; i<this.highscore.size(); i++)
+		{
+			Score sc = (Score) this.highscore.elementAt(i);
+			s.executeQuery("insert into highscore values('"+sc.getId()+"', '"
+													+sc.getNick()+"', '"+sc.getScore()+"");
+		}
 	}
 	
 	/**
-	 * Method for loading Highscore data
+	 * Method for loading Highscore data from the database
 	 *
 	 */
-	private void loadHighscore()
+	private void loadHighscore() throws SQLException
 	{
-		// empty
+		Statement s = con.createStatement();
+		ResultSet rs = s.executeQuery("select nick, score from highscore order by score desc");
+		
+		while(rs.next())
+		{
+			Score sc = new Score(rs.getInt("id"), rs.getString("nick"), rs.getInt("score"));
+			this.highscore.add(sc);
+		}
+		
+		// For now, we're just deleting all data from the database, all logic done by this class
+		rs = s.executeQuery("delete * from highscore");
 	}
-
 
 	/**
 	 * @return Returns the lowestScore.
@@ -109,10 +153,81 @@ public class HighScore
 	private int getLowestScore() {
 		return lowestScore;
 	}
+	
 	/**
 	 * @param lowestScore The lowestScore to set.
 	 */
 	private void setLowestScore(int lowestScore) {
 		this.lowestScore = lowestScore;
+	}
+	
+	/**
+	 * @return Returns the dbHost.
+	 */
+	private String getDbHost() {
+		return dbHost;
+	}
+	
+	/**
+	 * @param dbHost The dbHost to set.
+	 */
+	private void setDbHost(String dbHost) {
+		this.dbHost = dbHost;
+	}
+	
+	/**
+	 * @return Returns the dbName.
+	 */
+	private String getDbName() {
+		return dbName;
+	}
+	
+	/**
+	 * @param dbName The dbName to set.
+	 */
+	private void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+	
+	/**
+	 * @return Returns the dbPassword.
+	 */
+	private String getDbPassword() {
+		return dbPassword;
+	}
+	
+	/**
+	 * @param dbPassword The dbPassword to set.
+	 */
+	private void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
+	}
+	
+	/**
+	 * @return Returns the dbUri.
+	 */
+	private String getDbUri() {
+		return dbUri;
+	}
+	
+	/**
+	 * @param dbUri The dbUri to set.
+	 */
+	private void setDbUri(String dbUri) {
+		this.dbUri = dbUri;
+	}
+	
+	/**
+	 * @return Returns the dbUser.
+	 */
+	private String getDbUser() {
+		return dbUser;
+	}
+	
+	/**
+	 * @param dbUser The dbUser to set.
+	 */
+	private void setDbUser(String dbUser) {
+		this.dbUser = dbUser;
 	}
 }
